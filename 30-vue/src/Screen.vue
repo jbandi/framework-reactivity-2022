c4ced0e87e6035df8e9bfead330e500b6d9d8b82
Jonas Bandi <blog@jonasbandi.net>
2022-10-19 01:11:09 +0200

    Demo code

---
+++ 30-vue/src/Screen.vue
@@ -0,0 +1,10 @@
+<template>
+  <h3>Display of Screen</h3>
+  <Counter />
+</template>
+
+<script setup lang="ts">
+import Counter from "./Counter.vue";
+</script>
+
+<style scoped></style>
---
+++ 40-svelte/src/Screen.svelte
@@ -0,0 +1,7 @@
+<script>
+import Counter from './Counter.svelte';
+</script>
+<div>
+    <h3>Display of Screen!</h3>
+    <Counter />
+</div>
